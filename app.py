dd291cc76b9daaf727205a090718426d8a388e72
omar <omaryared123@icloud.com>
2024-03-09 17:41:20 -0600

    initial commit

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ import pandas as pd
 import streamlit as st 
 import plotly.express as px 
 
-data_vehicle = pd.read_csv('vehicles_us.csv')
+data_vehicle = pd.read_csv('~/proyecto5/vehicles_env/AVAMS/vehicles_us.csv')
 
 st.header('my aplicacion web')
 
 his_button = st.button('construir un histograma')
 
-his_button2 = st.button('construir un grafica de dispercion')
+
 
 if his_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(data_vehicle, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+his_button2 = st.button('construir un grafica de dispercion')
 
 if his_button2:
-    st.write('creando grafica de dispercion')
-
-fig = px.histogram(data_vehicle, x='odometer')
-
-st.plotly_chart(fig, use_container_width=True)
+    st.write('grafica de dispercion creado')
+    fig2 = px.scatter(data_vehicle, x='odometer')
+    st.plotly_chart(fig2, use_container_width=True)
