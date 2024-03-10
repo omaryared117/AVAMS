45d700a458db311a2abeedf2168ca6e094574f2d
omar <omaryared123@icloud.com>
2024-03-09 21:02:26 -0600

    initial commit

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ his_button = st.button('construir un histograma')
 
 if his_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    fig = px.histogram(data_vehicle, x='odometer')
+    table = data_vehicle.pivot_table(index='model',columns='condition', values='model_year')
+    fig = px.histogram(table)
     st.plotly_chart(fig, use_container_width=True)
 
 his_button2 = st.button('construir un grafica de dispercion')
 
 if his_button2:
     st.write('grafica de dispercion creado')
-    fig2 = px.scatter(data_vehicle, x='odometer')
+    table = data_vehicle.pivot_table(index='model_year',columns='condition', values='price')
+    fig2 = px.scatter(table)
     st.plotly_chart(fig2, use_container_width=True)
