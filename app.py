14b74025ca29419bcb948087302f4941e56ffc98
omar <omaryared123@icloud.com>
2024-03-10 21:10:41 -0600

    initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 data_vehicle = pd.read_csv('vehicles_us.csv')
 
-st.header('my aplicacion web')
+st.header('analisis de precio de modelos de autos')
 
 his_button = st.button('construir un histograma')
 
@@ -12,14 +12,15 @@ his_button = st.button('construir un histograma')
 
 if his_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    table = data_vehicle.pivot_table(index='model',columns='condition', values='model_year')
-    fig = px.histogram(table)
+    fig = px.histogram(data_vehicle, x='price', color='model')
+    fig.show()
     st.plotly_chart(fig, use_container_width=True)
 
 his_button2 = st.button('construir un grafica de dispercion')
 
 if his_button2:
-    st.write('grafica de dispercion creado')
-    table = data_vehicle.pivot_table(index='model_year',columns='condition', values='price')
-    fig2 = px.scatter(table)
+    st.write('creacion de grafica de dispercion para entre odommetros y años de los modelos')
+    fig2 = px.scatter(data_vehicle, x='odometer', y='model_year', color='model')
+    fig2.show()
+
     st.plotly_chart(fig2, use_container_width=True)
